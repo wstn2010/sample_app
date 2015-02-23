d926ae87cec900c4d5c238f6ea0f9be296c03445
kenputer <wstn2010@gmail.com>
2015-02-23 12:21:46 +0900

    fixed string -> text

--- db/migrate/20150119124907_add_goods_to_microposts.rb
+++ db/migrate/20150119124907_add_goods_to_microposts.rb
@@ -5,10 +5,10 @@ class AddGoodsToMicroposts < ActiveRecord::Migration
     add_column :microposts, :category, :string
     add_column :microposts, :whole_price, :integer
     add_column :microposts, :shipping_cost, :integer
-    add_column :microposts, :short_desc, :string
-    add_column :microposts, :desc, :string
-    add_column :microposts, :rep_short_desc, :string
-    add_column :microposts, :rep_desc, :string
+    add_column :microposts, :short_desc, :text
+    add_column :microposts, :desc, :text
+    add_column :microposts, :rep_short_desc, :text
+    add_column :microposts, :rep_desc, :text
 
     add_index :microposts, :key, unique: true
     add_index :microposts, :category
--- db/migrate/20150124111353_add_data_to_users.rb
+++ db/migrate/20150124111353_add_data_to_users.rb
@@ -1,7 +1,7 @@
 class AddDataToUsers < ActiveRecord::Migration
   def change
     add_column :users, :rep_id, :string
-    add_column :users, :introduction, :string
+    add_column :users, :introduction, :text
 
     add_index :users, :rep_id, :unique => true
   end
