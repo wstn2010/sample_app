4fbb04ccbfaea83b02e9eba5e518aef7ba28239a
kenputer <wstn2010@gmail.com>
2015-02-25 20:43:31 +0900

    rep mode

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,10 @@
 class StaticPagesController < ApplicationController
 
   def home
-    if signed_in?
-    	@micropost = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
-    else 
-      @todays_goods = MicropostsController.todays_goods 'food'
-      @todays_remaked1 = MicropostsController.goods_remarkable_pickups
-      @todays_remaked2 = MicropostsController.goods_remarkable_pickups
-      @todays_remaked3 = MicropostsController.goods_remarkable_pickups
-    end
+    @todays_goods = MicropostsController.todays_goods 'food'
+    @todays_remaked1 = MicropostsController.goods_remarkable_pickups
+    @todays_remaked2 = MicropostsController.goods_remarkable_pickups
+    @todays_remaked3 = MicropostsController.goods_remarkable_pickups
   end
 
   def help
