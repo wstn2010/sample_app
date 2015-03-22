5ca55ec3b32ae947cc7ea4764f535f2ce9f7ee94
kenputer <wstn2010@gmail.com>
2015-03-22 21:46:12 +0900

    login by rep-id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by(email: params[:session][:email].downcase)
+		user = User.find_by(rep_id: params[:session][:rep_id])
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
 			#redirect_back_or user
 			redirect_to root_url
 		else
-			flash.now[:error] = 'Invalid email/password combination'
+			flash.now[:error] = 'Invalid rep_id/password combination'
 			render 'new'
 		end
 	end
