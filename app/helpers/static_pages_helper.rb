9e72b96fc8c969e45be25f181e969c2a5ce70b28
kenputer <wstn2010@gmail.com>
2015-05-03 10:45:39 +0900

    fixed #3: 商材０は削除してはだめ

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -3,4 +3,15 @@ module StaticPagesHelper
 	def controllable?(goods)
 		current_user?(goods.user)
 	end
+
+	def deletable?(goods)
+		!zero_goods?(goods)
+	end
+
+	private
+
+		def zero_goods?(goods)
+			goods.goods_seq == 0
+		end
+
 end
